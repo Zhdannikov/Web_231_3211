9bd22f0ecc06df47724e64c5ca4e98a934b746da
Zhdannikov <zhdannikovarslan@gmail.com>
2024-10-13 21:29:30 +0300

    Lab 4

---
+++ Dishes.js
@@ -0,0 +1,73 @@
+const dishes = [
+{
+    keyword:'gazpacho',
+    name:'Гаспачо',
+    price: 195,
+    category:'soyp',
+    count: '350 г',
+    image: 'Gazpacho.jpg'
+},
+{
+    keyword:'Mushroom',
+    name:'Грибной суп-пюре',
+    price: 185,
+    category:'soyp',
+    count: '330 г',
+    image: 'Mushroom.jpg'
+},
+{
+    keyword:'Norwegian',
+    name:'Норвежский суп-пюре',
+    price: 270,
+    category:'soyp',
+    count: '330 г',
+    image: 'Norwegian.jpg'
+},
+{
+    keyword:'Potato',
+    name:'Жаренная картошка с грибами',
+    price: 150,
+    category:'the main course',
+    count: '250 г',
+    image: 'Potato.jpg'
+},
+{
+    keyword:'Lasagna',
+    name:'Лазанья',
+    price: 385,
+    category:'the main course',
+    count: '310 г',
+    image: 'Lasagna.jpg'
+},
+{
+    keyword:'Chops',
+    name:'Котлеты из курицы с картофельным пюре',
+    price: 225,
+    category:'the main course',
+    count: '280 г',
+    image: 'Chops.jpg'
+},
+{
+    keyword:'Orange',
+    name:'Апельсиновый сок',
+    price: 120,
+    category:'Drinks',
+    count: '300 мл',
+    image: 'Orange.jpg'
+},
+{
+    keyword:'Apple',
+    name:'Яблочный сок',
+    price: 90,
+    category:'Drinks',
+    count: '300 мл',
+    image: 'Apple.jpg'
+},
+{
+    keyword:'Carrot',
+    name:'Морковный сок',
+    price: 110,
+    category:'Drinks',
+    count: '300 мл',
+    image: 'Carrot.jpg'
+}];
