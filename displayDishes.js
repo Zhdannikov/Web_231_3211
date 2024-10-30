33ef20f2a8d08961bf2cee950726edbebd66dfc0
Zhdannikov <zhdannikovarslan@gmail.com>
2024-10-30 22:43:47 +0300

    Lab 5

--- displayDishes.js
+++ displayDishes.js
@@ -1,22 +1,45 @@
-// 2. Реализуйте отображение блюд на страницу с помощью JavaScript.
-// Необходимо написать скрипт, который будет перебирать массив с блюдами и выводить каждое из них в нужную секцию. Секции должны выглядеть также, 
-// как при статичном выводе из прошлых лабораторных.
-// - У каждого блока с блюдом должен быть data-атрибут "data-dish", в котором будет храниться название блюда на латинице.
-
 // добавляем обработчик событий и определяем переменные для секций
 document.addEventListener("DOMContentLoaded", () => {
     const soupSection = document.querySelector("#soup-section .dishes");
     const mainSection = document.querySelector("#main-section .dishes");
+    const salatSection = document.querySelector("#salat-section .dishes");
     const drinkSection = document.querySelector("#drink-section .dishes");
+    const desertSection = document.querySelector("#desert-section .dishes");   
+
+    //обработчик событий для кнопок фильтрации 
+    document.querySelectorAll(".filters button").forEach(filter => {
+        filter.addEventListener("click", (event) => {
+            const section = event.target.closest("section");
+            const previouslyActive = section.querySelector(".filters button.active");
+            if (previouslyActive === event.target) {
+                event.target.classList.remove("active");
+                filterDishes(section, "all");
+            } else {
+                section.querySelectorAll(".filters button").forEach(btn => btn.classList.remove("active"));
+                event.target.classList.add("active");
+                const kind = event.target.getAttribute("data-kind");
+                filterDishes(section, kind);
+            }
+        });
+    });
+
+    //фильтрация блюд
+    const filterDishes = (section, kind) => {
+        const sectionDishes = section.querySelectorAll(".dish");
+        sectionDishes.forEach(dishElement => {
+            const dishKeyword = dishElement.getAttribute("data-dish");
+            const dish = dishes.find(d => d.keyword === dishKeyword);
+            if (kind === "all" || dish.kind.includes(kind)) {
+                dishElement.style.display = "block";
+            } else {
+                dishElement.style.display = "none";
+            }
+        });
+    };
 
 
-
-    // 5.Отсортируйте блюда каждой категории в алфавитном порядке.
-    // - Используйте метод sort() перед отображением блюд на страницу. Блюда должны быть отсортированы в алфавитном порядке.
-    // сортируем массив объектов dishes по алфавиту по name 
+    //Сортировка блюда каждой категории в алфавитном порядке.
     dishes.sort((a, b) => a.name.localeCompare(b.name));
-
-
 
 
     dishes.forEach(dish => { // проходим по каждому объекту в массиве
@@ -36,31 +59,37 @@ document.addEventListener("DOMContentLoaded", () => {
         `;
 // добавление элемента блюда в нужную секцию
         if (dish.category === "soup") {
-            soupSection.appendChild(dishElement);
+             soupSection.appendChild(dishElement);
         } else if (dish.category === "main") {
             mainSection.appendChild(dishElement);
+        } else if (dish.category === "salat") {
+            salatSection.appendChild(dishElement);
         } else if (dish.category === "drink") {
             drinkSection.appendChild(dishElement);
+        } else if (dish.category === "desert") {  
+            desertSection.appendChild(dishElement);
         }
     });
 
     //определение элементов формы заказа
     const orderForm = {
         soup: document.getElementById("selected-soup"),
-        main: document.getElementById("selected-main-dish"),
+        main: document.getElementById("selected-main"),
+        salat: document.getElementById("selected-salat"),
         drink: document.getElementById("selected-drink"),
+        desert: document.getElementById("selected-desert"), 
         totalPrice: document.querySelector("#total-price .price-value")
     };
 // инициалиируем выбранные блюда
     let selectedDishes = {
         soup: null,
         main: null,
-        drink: null
+        salat: null,
+        drink: null,
+        desert: null
     };
 
 
-
-    
     //     4.Реализуйте подсчет итоговой стоимости для всех выбранных позиций меню.
     // После блоков с выбранными блюдами должен располагаться блок "Стоимость заказа".
     // В блоке отображается итоговая стоимость всех блюд. Например, если был выбран только напиток, отобразиться его цена:
@@ -82,7 +111,9 @@ document.addEventListener("DOMContentLoaded", () => {
         const noSelectionMessage = document.getElementById("no-selection-message");
         const soupOrder = document.getElementById("soup-order");
         const mainOrder = document.getElementById("main-order");
+        const salatOrder = document.getElementById("salat-order");
         const drinkOrder = document.getElementById("drink-order");
+        const desertOrder = document.getElementById("desert-order"); 
         const totalPriceBlock = document.getElementById("total-price-block");
 
         //отображение или скрытие элементов в зависимости выбрано блюдо или нет
@@ -90,13 +121,17 @@ document.addEventListener("DOMContentLoaded", () => {
             noSelectionMessage.style.display = 'block';
             soupOrder.style.display = 'none';
             mainOrder.style.display = 'none';
+            salatOrder.style.display = 'none';
             drinkOrder.style.display = 'none';
+            desertOrder.style.display = 'none';  
             totalPriceBlock.style.display = 'none';
         } else {
             noSelectionMessage.style.display = 'none';
             soupOrder.style.display = 'block';
             mainOrder.style.display = 'block';
+            salatOrder.style.display = 'block';
             drinkOrder.style.display = 'block';
+            desertOrder.style.display = 'block';   
             totalPriceBlock.style.display = 'block';
             orderForm.totalPrice.textContent = `${total}₽`;
         }
@@ -107,7 +142,9 @@ document.addEventListener("DOMContentLoaded", () => {
             noSelectionMessage.style.display = 'block';
             soupOrder.style.display = 'none';
             mainOrder.style.display = 'none';
+            salatOrder.style.display = 'none';
             drinkOrder.style.display = 'none';
+            desertOrder.style.display = 'none';  
             totalPriceBlock.style.display = 'none';
         })
     };
